7d02bffd0b93e9e79d62b50fcb7ed77a88581ffa
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-04-21 15:14:14 +0200

    First commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject obj-hierarchy "0.1.0-SNAPSHOT"
+  :description "A small library to explore objects hierarchies."
+  :url "https://github.com/manuelp/obj-hierarchy"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/obj_hierarchy/core_test.clj
@@ -0,0 +1,7 @@
+(ns obj-hierarchy.core-test
+  (:use clojure.test
+        obj-hierarchy.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
